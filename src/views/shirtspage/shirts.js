2ca7bf840c6e40044fbdff519d2c2eed69e343e9
walidismail11 <walid.ismail@net.usj.edu.lb>
2021-11-02 10:41:00 +0200

    some modification on shirts,dresses,shoes(page) a setup for the page

--- src/views/shirtspage/shirts.js
+++ src/views/shirtspage/shirts.js
@@ -5,25 +5,31 @@ import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
 import GridItem from "components/Grid/GridItem";
 import GridContainer from "components/Grid/GridContainer";
+import image1 from "assets/img/shirts.jpg";
+import dresses from "assets/img/dresses.jpg";
+import shoes from "assets/img/shoes.jpeg";
+import classNames from "classnames";
 
 const useStyles = makeStyles(styles);
 
 export default function aboutsection() {
   const classes = useStyles();
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
   return (
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem cs={12} sm={12} md={8}>
-          <h2 className={classes.title}>About us</h2>
-          <h4>
-            the quickstore is an online retail shop offers a wild variaty of
-            footwear and accesories from trendy fashion to casual for women
-          </h4>
-          <h2 className={classes.title}>Mission</h2>
-          <h4>
-            Our mission is to provide you with the unique shopping experience by
-            offering the latest trend with the professional and friendly service
-          </h4>
+          <h2>products</h2>
+          <h2>photo1</h2>
+          <img src={image1} alt="..." className={imageClasses} />
+          <h1>photo2</h1>
+          <img src={dresses} alt="..." className={imageClasses} />
+          <h1>photo3</h1>
+          <img src={shoes} alt="..." className={imageClasses} />
         </GridItem>
       </GridContainer>
     </div>
