4c3714b1c278ce1f97c23da11ccd57e694d2fcb1
walidismail11 <walid.ismail@net.usj.edu.lb>
2021-11-02 13:16:46 +0200

    modification in shirts page (redux)

--- src/views/shirtspage/shirts.js
+++ src/views/shirtspage/shirts.js
@@ -6,14 +6,18 @@ import styles from "assets/jss/material-kit-react/views/landingPageSections/work
 import GridItem from "components/Grid/GridItem";
 import GridContainer from "components/Grid/GridContainer";
 import image1 from "assets/img/shirts.jpg";
-import dresses from "assets/img/dresses.jpg";
-import shoes from "assets/img/shoes.jpeg";
+import dresses from "assets/img/shirt2.jpeg";
 import classNames from "classnames";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "actions";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles(styles);
 
 export default function aboutsection() {
   const classes = useStyles();
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   const imageClasses = classNames(
     classes.imgRaised,
     classes.imgRoundedCircle,
@@ -23,13 +27,14 @@ export default function aboutsection() {
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem cs={12} sm={12} md={8}>
-          <h2>products</h2>
+          <h2>lets make a difference</h2>
           <h2>photo1</h2>
           <img src={image1} alt="..." className={imageClasses} />
+          <h1>quantity{counter}</h1>
+          <Button onclick={() => dispatch(increment(1))}>+</Button>
+          <Button onclick={() => dispatch(increment(1))}>-</Button>
           <h1>photo2</h1>
           <img src={dresses} alt="..." className={imageClasses} />
-          <h1>photo3</h1>
-          <img src={shoes} alt="..." className={imageClasses} />
         </GridItem>
       </GridContainer>
     </div>
